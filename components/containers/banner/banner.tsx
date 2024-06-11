e44ce04c7c2836d64b8f0f61e03afd666179003b
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-06-11 18:12:57 +0200

    Update banner.tsx
    Updated text of banner

--- components/containers/banner/banner.tsx
+++ components/containers/banner/banner.tsx
@@ -2,9 +2,8 @@ export const Banner = () => {
   return (
     <section className="flex justify-center bg-foreground p-2">
       <span className="text-center text-sm font-light text-muted/70">
-        This is a reference implementation of the Grids data which is still in
-        ALPHA - Data is focused on Solana projects - API users can pick and show
-        data relevant to their users.
+        YOU SHOULD NOT USE THIS AS A DAILY DRIVER. This is a reference implementation of the Grids data which is still in
+        ALPHA - API users can pick and show data relevant to their users.
       </span>
     </section>
   );
