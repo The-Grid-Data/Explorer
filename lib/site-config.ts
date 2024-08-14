6ca689cc54cba668923f659ac5fa21d048cf63ba
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-08-14 17:55:05 +0200

    Update site-config.ts

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,8 +1,8 @@
 export const siteConfig = {
-  pageTitle: 'The Grid | Data Explorer',
+  pageTitle: 'Superteam Solana Explorer',
   pageDescription:
-    'Powering Discoverability of Assets, Products, and Services in Web3.',
-  filterByProductIds: [], // Used to filter which blockchains to show in the full list.
-  blockchainIds: [], // Filter default options. Filters the options of (productSupports, productDeployedOnFilter and assetDeployedOnFilter)
+    'Discover the Solana Ecosystem through Superteams Explorer',
+  filterByProductIds: [22], // Used to filter which blockchains to show in the full list.
+  blockchainIds: [22], // Filter default options. Filters the options of (productSupports, productDeployedOnFilter and assetDeployedOnFilter)
   blockchainProductTypeIds: [15, 16, 17] // Filter default options. used by productDeployedOnFilter.
 };
