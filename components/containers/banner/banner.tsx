ebae385b4e53bfe144e211814d6daef9a99ea5bb
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-08-14 17:55:58 +0200

    Update banner.tsx

--- components/containers/banner/banner.tsx
+++ components/containers/banner/banner.tsx
@@ -2,8 +2,7 @@ export const Banner = () => {
   return (
     <section className="flex justify-center bg-foreground p-2">
       <span className="text-center text-sm font-light text-muted/70">
-        YOU SHOULD NOT USE THIS AS A DAILY DRIVER. This is a reference implementation of the Grids data which is still in
-        ALPHA - API users can pick and show data relevant to their users.
+        Welcome to Superteams Solana Explorer! This is a first interation of this but more can come. There are issues in the github to work on! 
       </span>
     </section>
   );
