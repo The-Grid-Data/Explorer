69d1f393c2bc200f91d469f0ca26abca76655731
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-08-23 21:23:40 +0200

    Update hero.tsx

--- components/containers/hero/hero.tsx
+++ components/containers/hero/hero.tsx
@@ -4,14 +4,14 @@ export const Hero = () => {
   return (
     <section className="mx-auto flex max-w-4xl flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-12 container">
       <Badge className="my-2" variant="secondary">
-        Currently focused on Solana with some data on Degen, Farcaster & Base.
+        Focused on Solana.
       </Badge>
 
       <h1 className="scroll-m-20 text-balance text-center text-4xl font-extrabold leading-tight tracking-tight lg:text-5xl lg:leading-[1.1]">
-        Powering Discoverability of Assets, Products, and Services in Web3.
+        A node of Truth around information on Assets, Products, and Services in Solana.
       </h1>
       <p className="max-w-xl text-center text-lg font-light text-foreground">
-        We provide validated & structured Metadata to Data Platforms & LLMs.
+        Use the filters below (mainly Profile sector + Product Type) to sort through and discover the ecosystem. 
       </p>
     </section>
   );
