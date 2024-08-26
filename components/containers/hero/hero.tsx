c966295e7f4bcf64d48d2a2c3146b4c60ec21a9c
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-08-26 11:46:43 +0000

    Update

--- components/containers/hero/hero.tsx
+++ components/containers/hero/hero.tsx
@@ -2,16 +2,17 @@ import { Badge } from '@/components/ui/badge';
 
 export const Hero = () => {
   return (
-    <section className="mx-auto flex max-w-4xl flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-12 container">
+    <section className="container mx-auto flex max-w-4xl flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-12">
       <Badge className="my-2" variant="secondary">
-        This data is avaiable to machines and humans.
+        This data is available to machines and humans.
       </Badge>
 
       <h1 className="scroll-m-20 text-balance text-center text-4xl font-extrabold leading-tight tracking-tight lg:text-5xl lg:leading-[1.1]">
-        Search and Filter the SuperteamMY Marketmap. 
+        Search and Filter the Solana Ecosystem.
       </h1>
       <p className="max-w-xl text-center text-lg font-light text-foreground">
-        Search for names you know or use filters such as Profile Sectors and Product Types. 
+        Search for names you know or use filters such as Profile Sectors and
+        Product Types.
       </p>
     </section>
   );
