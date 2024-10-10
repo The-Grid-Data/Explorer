f6514b96b56bd4c221c859e9a28113344da263a5
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-10-10 11:20:07 +0000

    Update wording.

--- components/containers/hero/hero.tsx
+++ components/containers/hero/hero.tsx
@@ -4,14 +4,15 @@ export const Hero = () => {
   return (
     <section className="container mx-auto flex max-w-4xl flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-12">
       <Badge className="my-2 text-center" variant="secondary">
-        Focused on Solana.
+        For all of Web3 but currently focused on Solana.
       </Badge>
 
       <h1 className="scroll-m-20 text-balance text-center text-4xl font-extrabold leading-tight tracking-tight lg:text-5xl lg:leading-[1.1]">
-        A Node of Truth on Assets, Products, and Services in Solana.
+        A Node of Truth on Assets, Products, and Services in Web3.
       </h1>
       <p className="max-w-xl text-center text-lg font-light text-foreground">
-        Explore the largest offchain dataset for Solana projects, use the filters to find projects relevant to you. 
+        Explore the largest offchain dataset for Solana projects, use the
+        filters to find projects relevant to you.
       </p>
     </section>
   );
