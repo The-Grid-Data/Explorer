7088aff6e86828ec48f2f4f2693ed4367ed297df
Jordan Hatcher <64616433+jordanshatcher@users.noreply.github.com>
2024-12-18 11:58:17 +0100

    Update banner.tsx
    Clean up plus link to the terms that cover the explorer for the use of the endpoint.

--- components/containers/banner/banner.tsx
+++ components/containers/banner/banner.tsx
@@ -2,7 +2,7 @@ export const Banner = () => {
   return (
     <section className="flex justify-center bg-foreground p-2">
       <span className="text-center text-sm font-light text-muted/70">
-        YOU SHOULD NOT USE THIS AS A DAILY DRIVER. This is a reference implementation of the Grids data which is in Beta - API users can pick and show data relevant to their users.
+        YOU SHOULD NOT USE THIS AS A DAILY DRIVER. This is a reference implementation of [The Grid's data](https://thegrid.id/legal/web-services-terms) which is in Beta - API users can pick and show data relevant to their users.
       </span>
     </section>
   );
