3bd283e5269edc02bf8ab1aef5af020c0186d8b9
sanwalsulehri <sanwalsulehri077@gmail.com>
2025-02-04 18:48:23 +0500

    On hover of social links, show the name field, not the social type #78 solved

--- components/containers/profile-list/components/profile-card/profile-card.tsx
+++ components/containers/profile-list/components/profile-card/profile-card.tsx
@@ -104,7 +104,7 @@ export type ProfileCardCardProps = {
 
 export const ProfileCard = ({ profile: profileData }: ProfileCardCardProps) => {
   const profile = useFragment(ProfileCardFragment, profileData);
-  const validLogoUrl = profile.logo && profile.logo.startsWith('https://');
+  const validLogoUrl = profile.logo?.startsWith('https://');
   const { filters } = useProfileFiltersContext();
 
   return (
@@ -112,119 +112,124 @@ export const ProfileCard = ({ profile: profileData }: ProfileCardCardProps) => {
       <div className="relative mt-20 rounded-lg border-2 border-primary bg-card text-card-foreground shadow-sm dark:border-secondary">
         <div className="relative -mt-16 flex w-full flex-col items-start gap-3 lg:absolute lg:-top-16 lg:left-[-24px] lg:mt-0 lg:flex-row">
           <div className="border-1 -ml-6 w-fit shrink-0 rounded-xl border-2 border-primary bg-white shadow-lg hover:scale-105 dark:border-secondary lg:ml-0">
-            <Link href={paths.profile.detail(profile.root?.slug ?? '')}>
+            <Link href={paths.profile.detail(profile.root?.slug ?? '#')}>
               <Avatar className="h-[100px] w-[160px] min-w-[120px] rounded-xl p-2">
-                {validLogoUrl && (
+                {validLogoUrl ? (
                   <AvatarImage
                     className="object-scale-down"
                     src={profile.logo}
-                    alt={profile.name}
+                    alt={profile.name || 'Profile Logo'}
                   />
+                ) : (
+                  <AvatarFallback className="bg-white">No logo</AvatarFallback>
                 )}
-                <AvatarFallback className="bg-white">No logo</AvatarFallback>
               </Avatar>
             </Link>
           </div>
+
           <div className="flex w-full flex-col gap-3 px-4 lg:mt-7 lg:p-0">
             <div className="flex flex-row gap-2">
               <Link
                 className="flex w-fit items-center gap-2"
-                href={paths.profile.detail(profile.root?.slug ?? '')}
+                href={paths.profile.detail(profile.root?.slug ?? '#')}
               >
                 <h3 className="w-fit text-2xl font-bold hover:underline">
-                  {profile.name}
+                  {profile.name || 'Unnamed Profile'}
                 </h3>
-                {profile.root?.profileTags?.find(
+                {profile.root?.profileTags?.some(
                   tag => tag.tag?.id === siteConfig.verifiedTagId
                 ) && <Badge className="w-fit">Claimed</Badge>}
               </Link>
             </div>
+
             <div className="flex flex-col gap-4 lg:mr-[-16px] lg:flex-row">
               <div className="w-fit flex-1">
                 <UrlTypeIconLinks
                   urls={[
-                    extractUrls(profile.urls),
-                    extractSocialUrls(profile.root?.socials)
+                    extractUrls(profile.urls || []),
+                    extractSocialUrls(profile.root?.socials || [])
                   ]}
                 />
               </div>
               <Button className="w-full lg:w-fit" variant="default" asChild>
-                <Link href={paths.profile.detail(profile.root?.slug ?? '')}>
+                <Link href={paths.profile.detail(profile.root?.slug ?? '#')}>
                   More info
                 </Link>
               </Button>
             </div>
           </div>
         </div>
+
         <div className="flex flex-col gap-2 p-4 lg:pt-14">
           <div className="flex flex-wrap gap-2">
             <ProfileCardFeature
               label="Profile type"
-              value={profile.profileType?.name}
-              active={filters.profileTypeFilter.active}
+              value={profile.profileType?.name || '-'}
+              active={filters.profileTypeFilter?.active}
             />
             <ProfileCardFeature
               label="Profile Sector"
-              value={profile.profileSector?.name}
-              active={filters.profileSectorsFilter.active}
+              value={profile.profileSector?.name || '-'}
+              active={filters.profileSectorsFilter?.active}
             />
             <ProfileCardFeature
               label="Profile Status"
-              value={profile.profileStatus?.name}
-              active={filters.profileStatusesFilter.active}
+              value={profile.profileStatus?.name || '-'}
+              active={filters.profileStatusesFilter?.active}
             />
             <ProfileCardFeature
               label="Profile Founding Date"
               value={
                 profile.foundingDate
                   ? format(new Date(profile.foundingDate), 'MMM yyyy')
-                  : undefined
+                  : '-'
               }
-              active={filters.profileFoundingDateFilter.active}
+              active={filters.profileFoundingDateFilter?.active}
             />
             <ProfileCardFeature
               label="Main Product Type"
-              value={profile.mainProduct?.products?.at(0)?.productType?.name}
+              value={
+                profile.mainProduct?.products?.[0]?.productType?.name || '-'
+              }
             />
             <ProfileCardFeature
               label="Issued Assets"
               value={
-                Boolean(profile.root?.assets?.length) &&
-                profile.root?.assets?.map(asset => asset.ticker).join(', ')
+                profile.root?.assets?.length
+                  ? profile.root.assets.map(asset => asset.ticker).join(', ')
+                  : '-'
               }
             />
           </div>
+
           <div className="space-y-2">
-            <ProfileCardDataPoint label="Tagline" value={profile.tagLine} />
+            <ProfileCardDataPoint
+              label="Tagline"
+              value={profile.tagLine || '-'}
+            />
             <ProfileCardDataPoint
               label="Short Description"
-              value={profile.descriptionShort}
+              value={profile.descriptionShort || '-'}
             />
             <ProfileCardDataPoint
               label="Tags"
-              value={profile.root?.profileTags
-                ?.map(tag => tag?.tag?.name)
-                .filter(Boolean)
-                .join(', ')}
-              active={filters.tagsFilter.active}
+              value={
+                profile.root?.profileTags
+                  ?.map(tag => tag.tag?.name)
+                  .filter(Boolean)
+                  .join(', ') || '-'
+              }
+              active={filters.tagsFilter?.active}
             />
-            {/* <ProfileCardDataPoint
-              label="Product types"
-              value={profile.root?.products
-                ?.map(product => product.productType?.name)
-                .filter(Boolean)
-                .join(', ')}
-              active={filters.productTypesFilter.active}
-            /> */}
             <ProfileCardDataPoint
               label="Products"
               active={[
-                filters.productTypesFilter.active,
-                filters.productDeployedOnFilter.active,
-                filters.productLaunchDateFilter.active,
-                filters.productStatusFilter.active,
-                filters.supportsProductsFilter.active
-              ].some(value => value)}
+                filters.productTypesFilter?.active,
+                filters.productDeployedOnFilter?.active,
+                filters.productLaunchDateFilter?.active,
+                filters.productStatusFilter?.active,
+                filters.supportsProductsFilter?.active
+              ].some(Boolean)}
               className="items-start"
             >
               <div className="flex h-full flex-wrap gap-2">
@@ -250,14 +255,15 @@ export const ProfileCard = ({ profile: profileData }: ProfileCardCardProps) => {
                 )}
               </div>
             </ProfileCardDataPoint>
+
             <ProfileCardDataPoint
               label="Assets"
               active={[
-                filters.assetDeployedOnFilter.active,
-                filters.assetStandardFilter.active,
-                filters.assetTickerFilter.active,
-                filters.assetTypeFilter.active
-              ].some(value => value)}
+                filters.assetDeployedOnFilter?.active,
+                filters.assetStandardFilter?.active,
+                filters.assetTickerFilter?.active,
+                filters.assetTypeFilter?.active
+              ].some(Boolean)}
               className="items-start"
             >
               <div className="flex h-full flex-wrap gap-2">
@@ -287,3 +293,4 @@ export const ProfileCard = ({ profile: profileData }: ProfileCardCardProps) => {
     </div>
   );
 };
+
