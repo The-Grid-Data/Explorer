f4313f7a541de52f31d2a66d9fa93f8c09aed134
Jose Luis Peña te Winkel <josepetew@gmail.com>
2025-02-07 14:45:42 +0100

    refactor retrieve config functionality]

--- lib/config/config.schema.ts
+++ lib/config/config.schema.ts
@@ -1,83 +1,57 @@
 import { z } from 'zod';
-import defaultConfig from './default-config.json';
 
 export const configSchema = z.object({
   metadata: z.object({
-    title: z.string().default(defaultConfig.metadata.title),
-    description: z.string().default(defaultConfig.metadata.description)
+    title: z.string(),
+    description: z.string()
   }),
-  verifiedTagId: z.string().default(defaultConfig.verifiedTagId),
+  verifiedTagId: z.string(),
   banner: z.object({
-    text: z.string().default(defaultConfig.banner.text)
+    text: z.string()
   }),
   header: z.object({
     logoSrc: z.object({
-      dark: z.string().default(defaultConfig.header.logoSrc.dark),
-      light: z.string().default(defaultConfig.header.logoSrc.light)
+      dark: z.string(),
+      light: z.string()
     }),
     docsButton: z.object({
-      label: z.string().default(defaultConfig.header.docsButton.label),
-      href: z.string().url().default(defaultConfig.header.docsButton.href)
+      label: z.string(),
+      href: z.string().url()
     }),
     claimProfileButton: z.object({
-      label: z.string().default(defaultConfig.header.claimProfileButton.label),
-      href: z
-        .string()
-        .url()
-        .default(defaultConfig.header.claimProfileButton.href)
+      label: z.string(),
+      href: z.string().url()
     }),
     githubButton: z.object({
-      label: z.string().default(defaultConfig.header.githubButton.label),
-      href: z.string().url().default(defaultConfig.header.githubButton.href)
+      label: z.string(),
+      href: z.string().url()
     })
   }),
   pages: z.object({
     home: z.object({
       hero: z.object({
-        badge: z.string().default(defaultConfig.pages.home.hero.badge),
-        title: z.string().default(defaultConfig.pages.home.hero.title),
-        description: z
-          .string()
-          .default(defaultConfig.pages.home.hero.description)
+        badge: z.string(),
+        title: z.string(),
+        description: z.string()
       })
     })
   }),
   overrideFilterValues: z.object({
-    productDeployedOn: z
-      .array(z.string())
-      .default(defaultConfig.overrideFilterValues.productDeployedOn),
-    supportsProducts: z
-      .array(z.string())
-      .default(defaultConfig.overrideFilterValues.supportsProducts),
-    productTypes: z
-      .array(z.string())
-      .default(defaultConfig.overrideFilterValues.productTypes),
-    productAssetRelationships: z
-      .array(z.string())
-      .default(defaultConfig.overrideFilterValues.productAssetRelationships),
-    tags: z.array(z.string()).default(defaultConfig.overrideFilterValues.tags),
-    productIds: z
-      .array(z.string())
-      .default(defaultConfig.overrideFilterValues.productIds)
+    productDeployedOn: z.array(z.string()),
+    supportsProducts: z.array(z.string()),
+    productTypes: z.array(z.string()),
+    productAssetRelationships: z.array(z.string()),
+    tags: z.array(z.string()),
+    productIds: z.array(z.string())
   }),
   overrideOptionsFilterValues: z.object({
-    productTypes: z
-      .array(z.string())
-      .default(defaultConfig.overrideOptionsFilterValues.productTypes)
+    productTypes: z.array(z.string())
   }),
   featureFlags: z.object({
-    displayQueriesButtons: z
-      .boolean()
-      .default(defaultConfig.featureFlags.displayQueriesButtons),
-    allowHeroFiltersSearch: z
-      .boolean()
-      .default(defaultConfig.featureFlags.allowHeroFiltersSearch),
-    displayTagsFilter: z
-      .boolean()
-      .default(defaultConfig.featureFlags.displayTagsFilter),
-    displayPoweredBy: z
-      .boolean()
-      .default(defaultConfig.featureFlags.displayTagsFilter)
+    displayQueriesButtons: z.boolean().optional(),
+    allowHeroFiltersSearch: z.boolean().optional(),
+    displayTagsFilter: z.boolean().optional(),
+    displayPoweredBy: z.boolean().optional()
   })
 });
 
