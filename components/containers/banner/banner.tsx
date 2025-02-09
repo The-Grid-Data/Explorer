d6c28aca4d3eb2a1102f25f85057584933ef0855
Jose Luis Peña te Winkel <josepetew@gmail.com>
2025-02-09 23:24:13 +0100

    update banner colors

--- components/containers/banner/banner.tsx
+++ components/containers/banner/banner.tsx
@@ -2,9 +2,9 @@ import { siteConfig } from '@/lib/site-config';
 
 export const Banner = () => {
   return (
-    <section className="flex justify-center bg-foreground p-2">
+    <section className="flex justify-center bg-foreground p-2 dark:bg-foreground/10">
       <span
-        className="text-center text-sm font-light text-muted/70"
+        className="text-center text-sm font-light text-muted/70 dark:text-muted-foreground"
         dangerouslySetInnerHTML={{ __html: siteConfig.banner.text }}
       />
     </section>
