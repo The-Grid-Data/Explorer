d55670ae371f240de28a4bcb6c8fd8079d26ad26
JJ <155550137+JJKnegtel@users.noreply.github.com>
2025-12-19 16:25:29 +0100

    Feat/ Introduce And / OR
    As we need this for one directory

--- lib/utils/default-where-filter.ts
+++ lib/utils/default-where-filter.ts
@@ -5,55 +5,62 @@ import deepmerge from 'deepmerge';
 const getDefaultWhereFilter = (): ProfileInfosBoolExp => {
   const orConditions: ProfileInfosBoolExp[] = [];
 
+  // Check if we need special AND logic for productTypes + productAssetRelationships
+  const hasProductTypes = siteConfig.overrideFilterValues.productTypes?.length;
+  const hasProductAssetRelationships = siteConfig.overrideFilterValues.productAssetRelationships?.length;
+  const useSpecialAndCase = hasProductTypes && hasProductAssetRelationships;
+
   if (siteConfig.overrideFilterValues.productIds?.length) {
-    orConditions.push(
-      {
-        root: {
-          products: {
-            supportsProducts: {
-              supportsProductId: {
+    orConditions.push({
+      _or: [
+        {
+          root: {
+            products: {
+              supportsProducts: {
+                supportsProductId: {
+                  _in: siteConfig.overrideFilterValues.productIds
+                }
+              }
+            }
+          }
+        },
+        {
+          root: {
+            products: {
+              productDeployments: {
+                smartContractDeployment: {
+                  deployedOnId: {
+                    _in: siteConfig.overrideFilterValues.productIds
+                  }
+                }
+              }
+            }
+          }
+        },
+        {
+          root: {
+            products: {
+              id: {
                 _in: siteConfig.overrideFilterValues.productIds
               }
             }
           }
-        }
-      },
-      {
-        root: {
-          products: {
-            productDeployments: {
-              smartContractDeployment: {
-                deployedOnId: {
-                  _in: siteConfig.overrideFilterValues.productIds
+        },
+        {
+          root: {
+            assets: {
+              assetDeployments: {
+                smartContractDeployment: {
+                  deployedOnId: {
+                    _in: siteConfig.overrideFilterValues.productIds
+                  }
                 }
               }
             }
           }
         }
-      },
-      {
-        root: {
-          products: {
-            id: {
-              _in: siteConfig.overrideFilterValues.productIds
-            }
-          }
-        }
-      },
-      {
-        root: {
-          assets: {
-            assetDeployments: {
-              smartContractDeployment: {
-                deployedOnId: {
-                  _in: siteConfig.overrideFilterValues.productIds
-                }
-              }
-            }
-          }
-        }
-      }
-    );
+      ]
+    });
   }
 
   if (siteConfig.overrideFilterValues.tags?.length) {
@@ -68,32 +75,60 @@ const getDefaultWhereFilter = (): ProfileInfosBoolExp => {
     });
   }
 
-  if (siteConfig.overrideFilterValues.productAssetRelationships?.length) {
+  // Special case: when both productTypes AND productAssetRelationships are configured
+  // they must BOTH match (AND logic within products)
+  if (useSpecialAndCase) {
     orConditions.push({
       root: {
         products: {
-          productAssetRelationships: {
-            asset: {
-              ticker: {
-                _in: siteConfig.overrideFilterValues.productAssetRelationships
+          _and: [
+            {
+              productTypeId: {
+                _in: siteConfig.overrideFilterValues.productTypes
+              }
+            },
+            {
+              productAssetRelationships: {
+                asset: {
+                  ticker: {
+                    _in: siteConfig.overrideFilterValues.productAssetRelationships
+                  }
+                }
+              }
+            }
+          ]
+        }
+      }
+    });
+  } else {
+    // Independent filters: add them separately with OR logic
+    if (hasProductAssetRelationships) {
+      orConditions.push({
+        root: {
+          products: {
+            productAssetRelationships: {
+              asset: {
+                ticker: {
+                  _in: siteConfig.overrideFilterValues.productAssetRelationships
+                }
               }
             }
           }
         }
-      }
-    });
-  }
+      });
+    }
 
-  if (siteConfig.overrideFilterValues.productTypes?.length) {
-    orConditions.push({
-      root: {
-        products: {
-          productTypeId: {
-            _in: siteConfig.overrideFilterValues.productTypes
+    if (hasProductTypes) {
+      orConditions.push({
+        root: {
+          products: {
+            productTypeId: {
+              _in: siteConfig.overrideFilterValues.productTypes
+            }
           }
         }
-      }
-    });
+      });
+    }
   }
 
   return orConditions.length ? { _or: orConditions } : {};
