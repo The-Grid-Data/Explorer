6f28b41d0275a79582fa9471827bb1fb98ffcf63
JollyGrin <lol@dean.lol>
2026-01-22 17:32:10 +0100

    DEV-3196/fix starknet image cropping

--- components/containers/profile-list/components/profile-card/profile-card.tsx
+++ components/containers/profile-list/components/profile-card/profile-card.tsx
@@ -130,11 +130,7 @@ export const ProfileCard = ({ profile: profileData }: ProfileCardCardProps) => {
             <Link href={paths.profile.detail(profile.root?.slug ?? '')}>
               <Avatar className="h-[100px] w-[160px] min-w-[120px] rounded-xl p-2">
                 {validLogoUrl && (
-                  <AvatarImage
-                    className="object-scale-down"
-                    src={validLogoUrl}
-                    alt={profile.name}
-                  />
+                  <AvatarImage src={validLogoUrl} alt={profile.name} />
                 )}
                 <AvatarFallback className="bg-white">No logo</AvatarFallback>
               </Avatar>
